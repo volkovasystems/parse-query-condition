d647e0d002dec58a82051b3b50a89eb52c2ccff9
Regynald Ventura <regynaldventura@gmail.com>
2014-08-13 18:32:21 +0800

    Added parsing for piped condition (multiple conditions)

--- parse-query-condition.js
+++ parse-query-condition.js
@@ -7,7 +7,7 @@ var parseQueryCondition = function parseQueryCondition( condition, defaultRefere
 			}
 		@end-meta-configuration
 	*/
-	
+
 	var conditionList = condition.split( "@" );
 	var reference = conditionList[ 0 ];
 
@@ -17,34 +17,42 @@ var parseQueryCondition = function parseQueryCondition( condition, defaultRefere
 		reference = defaultReference || undefined;
 	}
 
-	conditionList = condition.split( "=" );
-	var queryKey = conditionList[ 0 ];
-	
-	if( queryKey == condition ){
-		var error = new Error( "invalid query key" );
-		console.error( error );
-		callback( error );
-
-	}else{
-		condition = conditionList[ 1 ];
-	}
-
-	if( !condition ){
-		var error = new Error( "invalid condition value" );
-		console.error( error );
-		callback( error );
-	}
-
-	var conditionValue = condition;
-
 	var queryCondition = {
 		"reference": reference,
-		"queryObject": { }	
+		"queryObject": { }
 	};
 
-	queryCondition.queryObject[ queryKey ] = conditionValue
+	conditionList = condition.split( "\|" );
+	
+	conditionList.forEach(function onEach( keyValue ){
+
+		conditionList = keyValue.split( "=" );
+		var queryKey = conditionList[ 0 ];
+
+		if( queryKey == condition ){
+			var error = new Error( "invalid query key" );
+			console.error( error );
+			//callback( error );
+		}else{
+			condition = conditionList[ 1 ];
+		}
+
+		if( !condition ){
+			var error = new Error( "invalid condition value" );
+			console.error( error );
+			//callback( error );
+		}
+
+		var conditionValue = condition;
+
+		queryCondition.queryObject[ queryKey ] = conditionValue
+	} );
 
 	return queryCondition;
 };
 
 module.exports = parseQueryCondition;
+
+var query = parseQueryCondition( "X@a=b" );
+//var query = parseQueryCondition( "X@a=b|c=d|e=f" );
+console.log( query );
